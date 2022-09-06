5f4073143ae89a37b0e8c0555ce19bfe5e62888d
Mooner510 <lmck510@gmail.com>
2022-09-06 23:34:58 +0900

    Fixed Shooter.cs

--- Assets/Scripts/Entity/Shooter.cs
+++ Assets/Scripts/Entity/Shooter.cs
@@ -1,16 +1,20 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Entity
 {
-    public class Shooter : MonoBehaviour
+    public class Shooter : MonoBehaviour, IShooter
     {
+        [SerializeField] private float arrowDamage;
         private void Update()
         {
-            if (Input.GetKey(KeyCode.Space))
-            {
-                //TODO: We Have to Add Shooting Code
-            }
+            if (Input.GetKey(KeyCode.Space)) Shoot();
+        }
+
+        public float GetArrowDamage() => arrowDamage;
+
+        public void Shoot()
+        {
+            //TODO: We Have to Add Shooting Code
         }
     }
 }
